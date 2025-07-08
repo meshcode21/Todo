bf63a8bf080daa83aec5bed6366550f69b6511d5
mahesh <maheshudas2077@gmail.com>
2025-07-08 14:30:16 +0545

    url based database connection using database_url from environment variable.

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,15 +1,17 @@
 import mysql from 'mysql2/promise';
 
-const db = await mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+// const db = await mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 db.getConnection()
   .then(() => console.log('Database connected successfully'))
